695b263e1d7bacc99ec33b2fe198d136cdcddf38
Jeffrey Marx <jeffrey_marx@yahoo.com>
2013-01-05 09:33:57 -0500

    Index file with basic plugin stuff

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: FourSquare Local Information Widget
+Description:  Shows recent checkin activity around certain geographic areas. Useful for neighborhood guides/hyperlocal blogs.
+Author: Jeff Marx
+*/
+
+function foursquare_local() {
+	
+}
+
