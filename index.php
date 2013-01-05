40d04c4b4ea889e60e29e5386e6fcf8fdf9681a8
Jeffrey Marx <jeffrey_marx@yahoo.com>
2013-01-05 18:58:47 -0500

    - Addition of field for spelled out location - Checking for latitude longitude. If that is not given, use the location. - Addition of several value checks to make sure key information is available before we move forward. - General cleanup stuff - Almost there next up. Put this into a wordpress widget and shortcode

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ function register_mysettings() {
 	register_setting( 'foursquare-local-group', 'client_id' );
 	register_setting( 'foursquare-local-group', 'client_secret' );	
 	register_setting( 'foursquare-local-group', 'll' );
+	register_setting( 'foursquare-local-group', 'location' );
 
 }
 
@@ -38,24 +39,34 @@ function foursquare_local_options() {
     <table class="form-table">
         <tr valign="top">
         <th scope="row">Client ID</th>
-        <td><input type="text" name="client_id" value="<?php echo get_option('client_id'); ?>" /></td>
+        <td><input size="65" type="text" name="client_id" value="<?php echo get_option('client_id'); ?>" /></td>
         </tr>
          
         <tr valign="top">
         <th scope="row">Client Secret</th>
-        <td><input type="text" name="client_secret" value="<?php echo get_option('client_secret'); ?>" /></td>
+        <td><input size="65" type="text" name="client_secret" value="<?php echo get_option('client_secret'); ?>" /></td>
+        </tr>
+			  <tr valign="top">
+        <th scope="row">Default location</th>
+        <td>
+			
+			Choose ONE of these methods to enter in your location<br>
+			<strong>Latitude/Longitude</strong>
+			<input type="text" name="ll" value="<?php echo get_option('ll'); ?>" /><br>
+			If you have an exact latitude/long (ex: 41.0141,-73.7552 ). You'll get the best results with this. If you don't have that, leave this field blank.
+			<p>
+			<strong>Location spelled out:</strong>
+			<input type="text" name="location" value="<?php echo get_option('location'); ?>" /><br>
+			Enter a city's name and state in here as you would on a letter.  (ex: White Plains, NY).
+			<p>		
+					
+		
+		</td>
         </tr>
 	</table>
-      
-	  <p>
-    
-	 Type in your location:<br>
-	 <input type="text" name="location" /><input type="submit" id="geo" value="Search!" /><br>
-	 <input type="text" name="ll" value="<?php echo get_option('ll'); ?>" />
-	
-    
+        
     <p class="submit">
-    <input type="submit" class="button-primary" value="<?php _e('Save Changess') ?>" />
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
     </p>
 
 </form>
@@ -69,29 +80,53 @@ function foursquare_local_options() {
 define("CLIENT_ID", get_option('client_id'));
 define("CLIENT_SECRET", get_option('client_secret'));
 define("LL", get_option('ll'));
+define("LOCATION", get_option('location'));
 
 /* display widget */	
 function foursquare_local() {	
 		
-	
-	echo "yo";
 	// Load the Foursquare API library
-	$foursquare = new FoursquareAPI(CLIENT_ID,CLIENT_SECRET);
-	$id = get_option('client_id');
+	$client_id = CLIENT_ID;
+	$client_secret = CLIENT_SECRET;
+	
+	if (empty($client_id) && empty($client_secret)) return;
+	
+	$foursquare = new FoursquareAPI($client_id, $client_secret);
+	$location = LOCATION;
+	$ll = LL;
+	
+	//If we don't have either of these values, no reason to go forward. Just bail out
+	if (empty($ll) && empty($location)) return;
 	
 	// Prepare parameters
-	$params = array("ll"=>LL,"section" => "food","venuePhotos" => 1);
+	if (!empty($ll)) {
+		$params = array("ll"=>LL,"section" => "food","venuePhotos" => 1, "limit" => 5);
+	} else {
+		$params = array("near"=>LOCATION,"section" => "food","venuePhotos" => 1, "limit" => 5);
+	}
+	/*
+	For the time being, we are going to favor a lat/long result over a spelled out letter type of result just because
+	we'll get better results, in the case that both fields are filled out. We could possibly change that though.
+	todo: Write some javascript on the options page to not allow the user to populate bothe fields. Hopefully people will not be idiots and read the instructions
+	carefully.
+	*/
+	
 	$response = $foursquare->GetPublic("venues/explore",$params);
+	//api call
+	
 	$venues = json_decode($response);
+	//response from api call
+	
 	$photourl = '';	
 	foreach($venues->response->groups as $group):
 	//loop through groups which only should be the one, "Recommended places"
 		foreach($group->items as $venue):	
 		//loop through the items within that group and represents the venues															
 				echo esc_html($venue->venue->name).'<br>';
-				$featuredphotos = $venue->venue->featuredPhotos->items;				
+				$featuredphotos = $venue->venue->featuredPhotos->items;
 				if (!empty($featuredphotos)) {
 					foreach($featuredphotos as $photo):
+					//Loop through the featured photos array. All we need is the url which is close to the top.
 						$photourl = $photo->url;
 					endforeach; ?>
 					<img width="100" height="100" src="<?php echo esc_html($photourl); ?>">	<p>
@@ -102,5 +137,6 @@ function foursquare_local() {
 				}
 		endforeach;
 	endforeach;
+	//wooh, all done. After all that looping i need a #beer.
 }
 
