8b80952c908b1191099483ed910d1a42808ec527
Jeff Marx <jeffrey_marx@yahoo.com>
2013-01-07 15:45:02 -0500

    Linking restaurant title to it's foursquare page

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ add_action( 'admin_menu', 'foursquare_local_menu' );
 function register_mysettings() {
 	//register our settings
 	register_setting( 'foursquare-local-group', 'client_id' );
-	register_setting( 'foursquare-local-group', 'client_secret' );	
+	register_setting( 'foursquare-local-group', 'client_secret' );
 	register_setting( 'foursquare-local-group', 'll' );
 	register_setting( 'foursquare-local-group', 'location' );
 
@@ -42,7 +42,7 @@ function foursquare_local_options() {
         <th scope="row">Client ID</th>
         <td><input size="65" type="text" name="client_id" value="<?php echo get_option('client_id'); ?>" /></td>
         </tr>
-         
+
         <tr valign="top">
         <th scope="row">Client Secret</th>
         <td><input size="65" type="text" name="client_secret" value="<?php echo get_option('client_secret'); ?>" /></td>
@@ -50,7 +50,7 @@ function foursquare_local_options() {
 			  <tr valign="top">
         <th scope="row">Default location</th>
         <td>
-			
+
 			Choose ONE of these methods to enter in your location<br>
 			<strong>Latitude/Longitude</strong>
 			<input type="text" name="ll" value="<?php echo get_option('ll'); ?>" /><br>
@@ -59,13 +59,13 @@ function foursquare_local_options() {
 			<strong>Location spelled out:</strong>
 			<input type="text" name="location" value="<?php echo get_option('location'); ?>" /><br>
 			Enter a city's name and state in here as you would on a letter.  (ex: White Plains, NY).
-			<p>		
-					
-		
+			<p>
+
+
 		</td>
         </tr>
 	</table>
-        
+
     <p class="submit">
     <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
     </p>
@@ -83,28 +83,28 @@ define("CLIENT_SECRET", get_option('client_secret'));
 define("LL", get_option('ll'));
 define("LOCATION", get_option('location'));
 
-/* display widget */	
-function foursquare_local($ll, $location) {	
+/* display widget */
+function foursquare_local($ll, $location) {
 
 	// Load the Foursquare API library
 	$client_id = CLIENT_ID;
 	$client_secret = CLIENT_SECRET;
-	
+
 	//If we don't have either of these values, no reason to go forward. Just bail out
 	if (empty($client_id) && empty($client_secret)) return;
-	
+
 	$foursquare = new FoursquareAPI($client_id, $client_secret);
-	
+
 	if (empty($location)) {
 		$location = LOCATION;
 	}
-	
+
 	if (empty($ll)) {
 		$ll = LL;
 	}
 	//If we don't have either of these values, no reason to go forward. Just bail out
 	if (empty($ll) && empty($location)) return;
-	
+
 	// Prepare parameters
 	if (!empty($ll)) {
 		$params = array("ll"=>$ll,"section" => "food","venuePhotos" => 1, "limit" => 5);
@@ -117,33 +117,36 @@ function foursquare_local($ll, $location) {
 	todo: Write some javascript on the options page to not allow the user to populate bothe fields. Hopefully people will not be idiots and read the instructions
 	carefully.
 	*/
-	
+
 	$response = $foursquare->GetPublic("venues/explore",$params);
 	//api call
-	
+
 	$venues = json_decode($response);
 	//response from api call
-	
-	$photourl = '';	
+
+	$photourl = '';
 	foreach($venues->response->groups as $group):
 	//loop through groups which only should be the one, "Recommended places"
-		foreach($group->items as $venue):	
-		//loop through the items within that group and represents the venues															
-				echo esc_html($venue->venue->name).'<br>';
+		foreach($group->items as $venue):
+		//loop through the items within that group and represents the venues
+				?>
+				<p>
+				<a target="_blank" href="<?php echo $venue->venue->canonicalUrl ?>"><?php echo esc_html($venue->venue->name) ?></a>
+				<br>
+				<?php
 				$featuredphotos = $venue->venue->featuredPhotos->items;
 				if (!empty($featuredphotos)) {
 					foreach($featuredphotos as $photo):
 					//Loop through the featured photos array. All we need is the url which is close to the top.
 						$photourl = $photo->url;
 					endforeach; ?>
-					<img width="100" height="100" src="<?php echo esc_html($photourl); ?>">	<p>
+					<img width="100" height="100" src="<?php echo esc_html($photourl); ?>">
 				<?php
-				} 
+				}
 				else {
-					 echo '<img style="border:1px solid black" src="' .plugins_url( 'images/nopic.png' , __FILE__ ). '" > <p>';
+					 echo '<img style="border:1px solid black" src="' .plugins_url( 'images/nopic.png' , __FILE__ ). '" >';
 				}
 		endforeach;
 	endforeach;
 	//wooh, all done. After all that looping i need a #beer.
 }
-
