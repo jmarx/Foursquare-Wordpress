405f69952b74d6a0bdc265ff10531fd2ba05ec6d
Jeffrey Marx <jeffmarx@gmail.com>
2013-10-29 13:52:58 -0400

    Addition of readme text on plugin file

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+/*
+
+-Plugin Name: FourSquare Local Explorer
+
+-Description:  Shows recent checkin activity around certain geographic areas. Useful for neighborhood guides/hyperlocal blogs.
+
+-Author: Jeff Marx
+
+-*/
+
 
 class Foursquare_Explorer {
 
